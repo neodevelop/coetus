bf513d0e7fc66d45db9082a0a368269d29348c1f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2008-07-16 22:11:30 +0000

    improved domain class

--- grails-app/domain/Event.groovy
+++ grails-app/domain/Event.groovy
@@ -1,7 +1,19 @@
 class Event {
+	String subject
+	EventType type
 	Date startTime
 	Date endTime
-	Boolean allDay
+	Boolean allDay = false
 	String location
-	String subject
+	Boolean necessaryRegistry = false
+	
+	static constraints = {
+		subject(blank:false,size:5..200)
+		type(nullable:false)
+		startTime(nullable:false)
+		endTime(nullable:false)
+		allDay(nullable:false)
+		location(blank:false,size:5..200)
+		necessaryRegistry(nullable:false)
+	}
 }
