f4f33ad6f2d51f4a4203ddae8825c7f824a4268c
Domingo Suarez Torres <domingo.suarez@gmail.com>
2008-07-29 05:23:37 +0000

    deleted

--- plugins/yui-2.5.2/scripts/_Upgrade.groovy
+++
@@ -1,2 +0,0 @@
-Ant.property(environment:"env")
-grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
--- plugins/yui-2.5.2/src/groovy/org/codehaus/groovy/grails/plugins/yui/Yui.groovy
+++
@@ -1,7 +0,0 @@
-package org.codehaus.groovy.grails.plugins.yui;
-
-class Yui {
-
-    static version = "2.5.2"
-    
-}
