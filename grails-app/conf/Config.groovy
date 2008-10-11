b26fbee3b0c691ef8283add7a661b12a1045a53c
Domingo Suarez Torres <domingo.suarez@gmail.com>
2008-10-11 20:43:33 +0000

    added configuration for mail plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -84,6 +84,19 @@ log4j {
 }
 
 
+grails {
+   mail {
+     host = "smtp.gmail.com"
+     port = 465
+     username = ""
+     password = ""
+     props = ["mail.smtp.auth":"true", 					   
+              "mail.smtp.socketFactory.port":"465",
+              "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+              "mail.smtp.socketFactory.fallback":"false"]
+   }
+}
+
 
 avatarPlugin {
 	defaultGravatarUrl="""http://www.ohloh.net/images/anon/anon80.gif"""
